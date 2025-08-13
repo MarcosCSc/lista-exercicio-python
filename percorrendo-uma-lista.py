fd3305424331a187375ccba61fb09b758bda8880
MarcosCSc <marcosandre@alunos.afya.com.br>
2025-08-12 21:17:35 -0300

    solução concluída do percorrendo uma lista

---
+++ percorrendo-uma-lista.py
@@ -0,0 +1,8 @@
+#Crie uma função que receba uma lista como argumento e percorra todos os seus elementos, imprimindo cada um na tela.
+numeros = [987654321,2,7654321,56,1234567, 1, 88888,3,42,999999,5,1000000000,13,101010,7,444, 9, 2, 13, 9]
+
+def imp_lista(lista):
+    for elemento in lista:
+        print(elemento)
+
+imp_lista(numeros)
