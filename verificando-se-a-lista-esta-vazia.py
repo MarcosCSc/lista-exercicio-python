c1381517a9fb5b41d1e42549cc386d347d0d7303
MarcosCSc <marcosandre@alunos.afya.com.br>
2025-08-12 22:29:04 -0300

    solucao concluida do verificando se a lista esta vazia

---
+++ verificando-se-a-lista-esta-vazia.py
@@ -0,0 +1,7 @@
+def lista_vazia(lista):
+    return len(lista) == 0
+
+lista1 = []
+lista2 = [11, 1239, 10329, 1, 0]
+print(f"A lista {lista1} está vazia? {lista_vazia(lista1)}")
+print(f"A lista {lista2} está vazia? {lista_vazia(lista2)}")
